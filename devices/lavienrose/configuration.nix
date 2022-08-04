93a6adae2e4179ba2bb5967f600f4ba4feaefe0f
Tristan Ross <tristan.ross@midstall.com>
2022-08-03 20:18:56 -0700

    feat(lavienrose): added pools

--- devices/lavienrose/configuration.nix
+++ devices/lavienrose/configuration.nix
@@ -3,15 +3,16 @@
 {
   # Bootloader
 
-  boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.enable = true;
+  boot.supportedFilesystems = [ "zfs" ];
+  boot.zfs.devNodes = "/dev/";
 
   # Initrd
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "uas" "usb_storage" "sd_mod" ];
 
   # Kernel
-  boot.kernelModules = [ "kvm-amd" ];
-  boot.kernelModules = [ "overlay" "nct6775" ];
+  boot.kernelModules = [ "kvm-amd" "overlay" "nct6775" ];
   boot.extraModprobeConfig = ''
     options kvm_intel nested=1
   '';
@@ -36,4 +37,57 @@
   # Services
   services.irqbalance.enable = true;
   services.ananicy.enable = true;
+
+  services.zfs = {
+    trim = {
+      enable = true;
+    };
+    autoScrub = {
+      enable = true;
+      pools = [ "rpool" ];
+    };
+    autoSnapshot = {
+      enable = true;
+      frequent = 8;
+      monthly = 1;
+    };
+  };
+
+  # Filesystems
+
+  fileSystems."/" =
+    { device = "rpool/nixos";
+      fsType = "zfs";
+      options = [ "zfsutil" ];
+    };
+
+  fileSystems."/backup" =
+    { device = "rpool/backup";
+      fsType = "zfs";
+      options = [ "zfsutil" ];
+    };
+
+  fileSystems."/nix" =
+    { device = "rpool/nixos/nix";
+      fsType = "zfs";
+      options = [ "zfsutil" ];
+    };
+
+  fileSystems."/home" =
+    { device = "rpool/userdata/home";
+      fsType = "zfs";
+      options = [ "zfsutil" ];
+    };
+
+  fileSystems."/root" =
+    { device = "rpool/userdata/home/root";
+      fsType = "zfs";
+      options = [ "zfsutil" ];
+    };
+
+  fileSystems."/home/ross" =
+    { device = "rpool/userdata/home/ross";
+      fsType = "zfs";
+      options = [ "zfsutil" ];
+    };
 }
