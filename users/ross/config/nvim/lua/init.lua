8d3cdb5ecc68b8d507a9ad4c314248641e91968d
Tristan Ross <tristan.ross@midstall.com>
2022-08-11 20:26:55 -0700

    feat: portable nvim setup

--- users/ross/config/nvim/lua/init.lua
+++ users/ross/config/nvim/lua/init.lua
@@ -1,119 +1,180 @@
-local lspconfig = require('lspconfig')
-local mason = require('mason')
-local cmp_lsp = require('cmp_nvim_lsp')
-local cmp = require('cmp')
-
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = cmp_lsp.update_capabilities(capabilities)
-
-vim.cmd('colorscheme tokyonight')
-
 vim.opt.number = true
 vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
 
-vim.api.nvim_set_keymap('n', '<leader>m', ':WinShift<CR>', { noremap = true })
+vim.cmd [[packadd packer.nvim]]
+
 vim.api.nvim_set_keymap('n', '<leader>q', ':exit<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>s', ':w<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>w', ':wq<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>g', ':Neogit<CR>', { noremap = true })
 
-vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>Telescope find_files<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>fg', '<cmd>Telescope live_grep<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>fb', '<cmd>Telescope buffers<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>fh', '<cmd>Telescope help_tags<CR>', { noremap = true })
-
-local on_attach = function(client, bufnr)
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-  local bufopts = { noremap = true, silent = true, buffer = bufnr }
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-  vim.keymap.set('n', '<space>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, bufopts)
-  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
-end
-
-local servers = { 'clangd', 'pyright', 'tsserver', 'rnix' }
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    capabilities = capabilities,
-    on_attach = on_attach
-  }
-end
-
-mason.setup()
-
-cmp.setup({
-  snippet = {
-    expand = function(args)
-      vim.fn["vsnip#anonymous"](args.body)
+return require('packer').startup(function(use)
+  use {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.0',
+    config = function()
+      vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>Telescope find_files<CR>', { noremap = true })
+      vim.api.nvim_set_keymap('n', '<leader>fg', '<cmd>Telescope live_grep<CR>', { noremap = true })
+      vim.api.nvim_set_keymap('n', '<leader>fb', '<cmd>Telescope buffers<CR>', { noremap = true })
+      vim.api.nvim_set_keymap('n', '<leader>fh', '<cmd>Telescope help_tags<CR>', { noremap = true })
     end,
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<CR>'] = cmp.mapping.confirm { select = true },
-    ['<C-e>'] = cmp.mapping.abort(),
-  }),
-  sources = cmp.config.sources({
-    { name = 'buffer' },
-    { name = 'nvim_lua' },
-    { name = 'nvim_lsp' },
-  })
-})
-
-cmp.setup.cmdline('/', {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = {
-    { name = 'buffer' }
   }
-})
 
-cmp.setup.cmdline(':', {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = cmp.config.sources({
-    { name = 'path' }
-  }, {
-    { name = 'cmdline' }
-  })
-})
+  use {
+    'williamboman/mason.nvim',
+    'williamboman/mason-lspconfig.nvim',
+    'neovim/nvim-lspconfig',
+    'hrsh7th/nvim-cmp',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
+    'hrsh7th/cmp-cmdline',
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-nvim-lua',
+    config = function()
+      local lspconfig = require('lspconfig')
+      local mlsp = require('mason-lspconfig')
+      local mason = require('mason')
+      local cmp_lsp = require('cmp_nvim_lsp')
+      local cmp = require('cmp')
 
-require('lspsaga').init_lsp_saga()
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities = cmp_lsp.update_capabilities(capabilities)
 
-require('dressing').setup({
-  input = {
-    enabled = true,
-  },
-  select = {
-    enabled = true,
-  },
-})
+      local on_attach = function(client, bufnr)
+        vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-require('gitsigns').setup({
-  current_line_blame = true,
-  current_line_blame_opts = {
-    virt_text = true,
-    virt_text_pos = 'eol',
-  },
-  current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
-  signcolumn = true,
-  numhl = true,
-})
+        local bufopts = { noremap = true, silent = true, buffer = bufnr }
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+        vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+        vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+        vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+        vim.keymap.set('n', '<space>wl', function()
+          print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+        end, bufopts)
+        vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+        vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+        vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+        vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+        vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+      end
 
-require('neogit').setup({
-  kind = "tab",
-  auto_refresh = true,
-  integrations = {
-    diffview = true,
-  },
-})
+      local servers = { 'clangd', 'pyright', 'tsserver', 'rnix' }
+      mlsp.setup_handlers({
+        function(name)
+          if vim.tbl_contains(servers, name) then
+           lspconfig[name].setup({
+              capabilities = capabilities,
+	      on_attach = on_attach,
+	    })
+          end
+	end,
+      })
+
+      mlsp.setup({
+        automatic_install = true,
+	ensure_installed = servers,
+      })
+
+      mason.setup()
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<CR>'] = cmp.mapping.confirm { select = true },
+          ['<C-e>'] = cmp.mapping.abort(),
+        }),
+        sources = cmp.config.sources({
+          { name = 'buffer' },
+          { name = 'nvim_lua' },
+          { name = 'nvim_lsp' },
+        })
+      })
+
+      cmp.setup.cmdline('/', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = 'buffer' }
+        }
+      })
+
+      cmp.setup.cmdline(':', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = 'path' }
+        }, {
+          { name = 'cmdline' }
+        })
+      })
+    end
+  }
+
+  use {
+    'stevearc/dressing.nvim',
+    config = function() require('dressing').setup({}) end,
+  }
+
+  use {
+    'glepnir/lspsaga.nvim',
+    branch = 'main',
+    config = function() require('lspsaga').init_lsp_saga() end,
+  }
+
+  use {
+    'lewis6991/gitsigns.nvim',
+    config = function() require('gitsigns').setup({
+      current_line_blame = true,
+      current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = 'eol',
+      },
+      current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+      signcolumn = true,
+      numhl = true,
+    }) end,
+  }
+
+  use {
+    'nvim-lua/plenary.nvim',
+    'TimUntersberger/neogit',
+    'sindrets/diffview.nvim',
+    config = function()
+      require('neogit').setup({
+        kind = "tab",
+        auto_refresh = true,
+        integrations = {
+          diffview = true,
+        },
+      })
+
+      vim.api.nvim_set_keymap('n', '<leader>g', ':Neogit<CR>', { noremap = true })
+    end
+  }
+
+  use {
+    'sindrets/winshift.nvim',
+    config = function()
+      require('winshift').setup({})
+
+      vim.api.nvim_set_keymap('n', '<leader>m', ':WinShift<CR>', { noremap = true })
+    end,
+  }
+
+  use {
+    'folke/tokyonight.nvim',
+    config = function() vim.cmd [[colorscheme tokyonight]] end,
+  }
+
+  use { 'norcalli/nvim-colorizer.lua' }
+  use { 'gpanders/editorconfig.nvim' }
+  use { 'xiyaowong/nvim-cursorword' }
+  use { 'LnL7/vim-nix' }
+end)
--- users/ross/default.nix
+++ users/ross/default.nix
@@ -1,12 +1,4 @@
 { pkgs, ... }:
-let
-  nur = import (builtins.fetchTarball {
-    url = https://github.com/nix-community/NUR/archive/6600601c83e9404c2dc5a848c4eb65b0beb9f298.zip;
-    sha256 = "1xa7cfzjph965a6jlla5s61srflijpz48lzq27m7x0qym5xq9r6q";
-  }) {
-    inherit pkgs;
-  };
-in
 {
   users.users.ross = {
     isNormalUser = true;
@@ -33,25 +25,8 @@ in
       withNodeJs = true;
       withPython3 = true;
       plugins = with pkgs.vimPlugins; [
-        nur.repos.m15a.vimExtraPlugins.mason-nvim
-        colorizer
-        cmp-nvim-lua
-        cmp-nvim-lsp
-        diffview-nvim
-        dressing-nvim
-        editorconfig-nvim
+        packer-nvim
         fcitx-vim
-        gitsigns-nvim
-        lspsaga-nvim
-        neogit
-        nvim-lspconfig
-        nvim-cmp
-        telescope-nvim
-        tokyonight-nvim
-        vim-vsnip
-        vim-nix
-        vim-cursorword
-        winshift-nvim
       ];
       extraConfig = ''
         lua require("init")
