02e59e1e2223b7f9c215528a9c9e871f5e49218c
Tristan Ross <tross@ezoic.com>
2022-08-16 16:59:59 -0700

    feat: status bar is okay on macos, globally added zsh

---
+++ users/ross/darwin-defaults.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+brew install koekeishiya/formulae/yabai
+brew install koekeishiya/formulae/skhd
+brew install cmacrae/formulae/spacebar
+
+brew services start spacebar
+brew services start skhd
+brew services start yabai
+
+defaults write com.apple.spaces spans-displays -bool false
+defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock "mru-spaces" -bool "false"
+defaults write com.apple.screencapture location -string "$HOME/Desktop"
+defaults write com.apple.screencapture disable-shadow -bool true
+defaults write com.apple.screencapture type -string "png"
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+defaults write NSGlobalDomain _HIHideMenuBar -bool true
+defaults write NSGlobalDomain AppleHighlightColor -string "0.615686 0.823529 0.454902"
+defaults write NSGlobalDomain AppleAccentColor -int 1
+defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
+defaults write com.apple.finder ShowStatusBar -bool false
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool false
--- users/ross/home.nix
+++ users/ross/home.nix
@@ -1,30 +1,42 @@
 { config, pkgs, ... }:
 {
-    home.packages = with pkgs; [
-      jq
-      btop
+  home.packages = with pkgs; [
+    jq
+    btop
+    neofetch
+  ];
+  home.sessionVariables.EDITOR = "nvim";
+  programs.neovim = {
+    enable = true;
+    withNodeJs = true;
+    withPython3 = true;
+    plugins = with pkgs.vimPlugins; [
+      packer-nvim
+      fcitx-vim
     ];
-    home.sessionVariables.EDITOR = "nvim";
-    programs.neovim = {
+    extraConfig = ''
+      lua require("init")
+    '';
+  };
+  programs.bash = {
+    enable = true;
+  };
+  programs.git = {
+    userEmail = "tristan.ross@midstall.com";
+    userName = "Tristan Ross";
+    extraConfig = {
+      core.pager = "nvimpager";
+    };
+  };
+  programs.zsh = {
+    enable = true;
+    enableAutosuggestions = true;
+    enableCompletion = true;
+    enableSyntaxHighlighting = true;
+    enableVteIntegration = true;
+    oh-my-zsh = {
       enable = true;
-      withNodeJs = true;
-      withPython3 = true;
-      plugins = with pkgs.vimPlugins; [
-        packer-nvim
-        fcitx-vim
-      ];
-      extraConfig = ''
-        lua require("init")
-      '';
+      theme = "tjkirch";
     };
-    programs.bash = {
-      enable = true;
-    };
-    programs.git = {
-      userEmail = "tristan.ross@midstall.com";
-      userName = "Tristan Ross";
-      extraConfig = {
-        core.pager = "nvimpager";
-      };
-    };
+  };
 }
