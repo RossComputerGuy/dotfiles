4babd140fea036b508b5ad032087c3fa83b70b50
Tristan Ross <tross@ezoic.com>
2022-08-16 21:53:34 -0700

    feat: use zsh on linux

--- users/ross/default.nix
+++ users/ross/default.nix
@@ -5,6 +5,7 @@
     home = "/home/ross";
     description = "Tristan Ross";
     extraGroups = [ "wheel" "docker" "adbusers" "games" "input" ];
+    shell = pkgs.zsh;
   };
 
   home-manager.users.ross = import ./home-linux.nix { inherit pkgs; inherit config; };
