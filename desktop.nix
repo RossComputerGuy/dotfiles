0f7931b436bf8214987cb5857bfd8823ca3e1a84
Tristan Ross <tristan.ross@midstall.com>
2022-11-22 14:14:02 -0800

    feat: enable tailscale

--- desktop.nix
+++ desktop.nix
@@ -117,6 +117,7 @@ in
   services.avahi.nssmdns = true;
 
   # Security
+  services.tailscale.enable = true;
   services.gnome.gnome-keyring.enable = true;
   services.udev.packages = [ pkgs.yubikey-personalization ];
   programs.ssh.askPassword = lib.mkForce "";
