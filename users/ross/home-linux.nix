6fab6d995afe1e8c131009ff09d73baa0723b7d3
Tristan Ross <tristan.ross@midstall.com>
2023-01-05 11:33:22 -0800

    feat: add dbus sway

--- users/ross/home-linux.nix
+++ users/ross/home-linux.nix
@@ -37,6 +37,7 @@ in
   home.username = "ross";
   home.homeDirectory = mkForce "/home/ross";
   home.packages = with pkgs; [
+    dbus-sway-environment
     eww-wayland
     nvimpager
     xdg-user-dirs
