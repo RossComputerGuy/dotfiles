e7772f7a455b856a6155722243d3dc3e9d095838
Tristan Ross <tristan.ross@midstall.com>
2024-01-03 13:48:39 -0800

    feat: remove hip

--- devices/lavienrose/default.nix
+++ devices/lavienrose/default.nix
@@ -55,10 +55,6 @@ in
   services.xserver.enable = true;
   services.xserver.videoDrivers = [ "amdgpu" ];
 
-  systemd.tmpfiles.rules = [
-    "L+    /opt/rocm/hip   -    -    -     -    ${pkgs.hip}"
-  ];
-
   hardware.opengl.driSupport = true;
   hardware.opengl.driSupport32Bit = true;
 
