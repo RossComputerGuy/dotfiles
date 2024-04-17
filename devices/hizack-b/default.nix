cd29a17d6290dfccf5b80a2a81a9beab07e4a49c
Tristan Ross <tristan.ross@midstall.com>
2024-04-16 22:27:51 -0700

    chore: update

--- devices/hizack-b/default.nix
+++ devices/hizack-b/default.nix
@@ -7,8 +7,6 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = false;
 
-  virtualisation.waydroid.enable = true;
-
   boot.binfmt = {
     emulatedSystems = [
       "x86_64-linux"
