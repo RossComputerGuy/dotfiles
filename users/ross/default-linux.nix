314dfd3ac2351a9e76533037f302c65be91a5aca
Tristan Ross <tristan.ross@midstall.com>
2025-07-18 19:28:28 -0700

    feat: add ross to dialout group

--- users/ross/default-linux.nix
+++ users/ross/default-linux.nix
@@ -1,7 +1,7 @@
 { config, lib, ... }: {
   name = "ross";
   home = "/home/ross";
-  extraGroups = [ "wheel" "docker" "games" "input" "video" ]
+  extraGroups = [ "wheel" "docker" "games" "input" "video" "dialout" ]
     ++ lib.optional config.programs.adb.enable "adbusers";
   isNormalUser = true;
 }
