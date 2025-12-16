3af40342e34d4f5a3c83b275436e0daea113cc23
Tristan Ross <tristan.ross@midstall.com>
2025-12-15 20:15:39 -0800

    feat(ross): use ghostty

--- users/ross/home.nix
+++ users/ross/home.nix
@@ -13,6 +13,37 @@ in
   home.stateVersion = "24.05";
   home.sessionVariables.EDITOR = "nvim";
   programs.home-manager.enable = true;
+  programs.ghostty = {
+    enableZshIntegration = true;
+    enable = true;
+    settings.theme = "tokyonight";
+    themes.tokyonight = {
+      background = "#1a1b26";
+      foreground = "#c0caf5";
+      selection-background = "#283457";
+      selection-foreground = "#c0caf5";
+      cursor-color = "#c0caf5";
+      cursor-text = "#1a1b26";
+      palette = [
+        "0=#15161e"
+        "1=#f7768e"
+        "2=#9ece6a"
+        "3=#e0af68"
+        "4=#7aa2f7"
+        "5=#bb9af7"
+        "6=#7dcfff"
+        "7=#a9b1d6"
+        "8=#414868"
+        "9=#f7768e"
+        "10=#9ece6a"
+        "11=#e0af68"
+        "12=#7aa2f7"
+        "13=#bb9af7"
+        "14=#7dcfff"
+        "15=#c0caf5"
+      ];
+    };
+  };
   programs.nixvim = {
     enable = pkgs.stdenv.hostPlatform == pkgs.stdenv.buildPlatform;
     colorschemes.tokyonight.enable = true;
