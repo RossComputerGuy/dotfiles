6e09b1edc47165c0a9e2a25f9f91481c113eb230
Tristan Ross <tristan.ross@determinate.systems>
2026-02-22 10:37:43 -0800

    feat: widevine on firefox

--- modules/programs.nix
+++ modules/programs.nix
@@ -1,4 +1,9 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
   inherit (config.ross) profile;
 in
@@ -11,17 +16,34 @@ in
       enable = true;
       enableSSHSupport = true;
     };
-    firefox.enable = !pkgs.stdenv.hostPlatform.isRiscV;
+    firefox = {
+      enable = !pkgs.stdenv.hostPlatform.isRiscV;
+      package = pkgs.firefox.overrideAttrs (old: {
+        buildCommand = old.buildCommand + ''
+          mkdir -p $out/gmp-widevinecdm/system-installed
+          ln -s "${pkgs.widevine-cdm}/share/google/chrome/WidevineCdm/_platform_specific/linux_arm64/libwidevinecdm.so" $out/gmp-widevinecdm/system-installed/libwidevinecdm.so
+          ln -s "${pkgs.widevine-cdm}/share/google/chrome/WidevineCdm/manifest.json" $out/gmp-widevinecdm/system-installed/manifest.json
+          wrapProgram "$oldExe" \
+            --set MOZ_GMP_PATH "$out/gmp-widevinecdm/system-installed"
+        '';
+      });
+    };
   };
 
-  environment.systemPackages = with pkgs; [
-    lm_sensors
-    nixpkgs-review
-  ] ++ lib.optionals (!pkgs.stdenv.hostPlatform.isRiscV64) [
-    pkgs.nix-output-monitor
-    pkgs.nix-diff
-    pkgs.nixfmt-rfc-style
-    pkgs.fwupd-efi
-    pkgs.android-tools
-  ] ++ lib.optional (stdenv.hostPlatform == stdenv.buildPlatform && profile == "desktop") papirus-icon-theme;
+  environment.systemPackages =
+    with pkgs;
+    [
+      lm_sensors
+      nixpkgs-review
+    ]
+    ++ lib.optionals (!pkgs.stdenv.hostPlatform.isRiscV64) [
+      pkgs.nix-output-monitor
+      pkgs.nix-diff
+      pkgs.nixfmt-rfc-style
+      pkgs.fwupd-efi
+      pkgs.android-tools
+    ]
+    ++ lib.optional (
+      stdenv.hostPlatform == stdenv.buildPlatform && profile == "desktop"
+    ) papirus-icon-theme;
 }
